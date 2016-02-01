0115626421c4218ba6828e13d5a700081178a07d
Alberto Mancini <alberto@jooink.com>
2016-02-01 16:12:16 +0100

    dropping unised classes with JsInterop

--- gwt-webgl/src/com/googlecode/gwtwebgl/utils/JsMixinBase.java
+++
@@ -1,8 +0,0 @@
-package com.googlecode.gwtwebgl.utils;
-
-import com.google.gwt.core.client.JavaScriptObject;
-
-public class JsMixinBase  extends JavaScriptObject {
-	protected JsMixinBase() {}
-	
-}
