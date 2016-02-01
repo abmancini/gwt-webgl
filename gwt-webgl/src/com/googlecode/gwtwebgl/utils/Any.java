75a0e5c47f7c4ec5adb49ad2d7b44b2595d13189
Alberto Mancini <alberto@jooink.com>
2016-02-01 16:32:55 +0100

    cleanup

--- gwt-webgl/src/com/googlecode/gwtwebgl/utils/Any.java
+++ gwt-webgl/src/com/googlecode/gwtwebgl/utils/Any.java
@@ -11,99 +11,75 @@ import com.googlecode.gwtwebgl.html.WebGLRenderbuffer;
 import com.googlecode.gwtwebgl.html.WebGLTexture;
 import com.googlecode.gwtwebgl.html.WebGLBuffer;
 
-public class Any extends JavaScriptObject /*implements Any */{
+public class Any extends JavaScriptObject {
 	protected Any() {}
 	
-	////@Override
 	public final native int getInt() /*-{
 		return this;
 	}-*/;
 	
-	////@Override
 	public final native boolean getBoolean() /*-{
 		return this;
 	}-*/;
 	
-	////@Override
 	public final native float getFloat() /*-{
 		return this;
 	}-*/;
 
-	//@Override
 	public final native double getDouble() /*-{
 		return this;
 	}-*/;
 	
-	//@Override
 	public final native Uint32Array getUint32Array() /*-{
 		return this;
 	}-*/;
 
-	//@Override
 	public final native Int32Array getInt32Array() /*-{
 		return this;
 	}-*/;
 
-	//@Override
 	public final native Float32Array getFloat32Array() /*-{
 		return this;
 	}-*/;
 
-	//@Override
 	public final native WebGLBuffer getWebGLBuffer() /*-{
 		return this;
 	}-*/;
 
-	
-	//@Override
 	public final native WebGLProgram getWebGLProgram() /*-{
 		return this;
 	}-*/;
 
-
-	//@Override
 	public final native WebGLTexture getWebGLTexture() /*-{
 		return this;
 	}-*/;
 
-
-	//@Override
 	public final native WebGLRenderbuffer getWebGLRenderbuffer() /*-{
 		return this;
 	}-*/;
 
-
-	//@Override
 	public final native WebGLFramebuffer getWebGLFramebuffer() /*-{
 		return this;
 	}-*/;
 
-	//@Override
 	public final native JsArrayBoolean getJsArrayBoolean() /*-{
 		return this;
 	}-*/;
 
-
-	//@Override
 	public final native Object get() /*-{
 		return this;
 	}-*/;
 
-	//@Override
 	public final native boolean isNull() /*-{
 		return this==null;
 	}-*/;
 
-
-	//@Override
 	public final native String getString() /*-{
 		return this;
 	}-*/;
 
-	//@Override
 	public final native String castToString() /*-{
 		return ''+this;
 	}-*/;
-
 
 }
